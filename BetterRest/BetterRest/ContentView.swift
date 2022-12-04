deabd0364ceeaa2521b57497124b721fde689bd5
Kurt <kurtanthonylane@hotmail.com>
2022-12-05 08:45:31 +1000

    Update to BetterRest
    Got BetterRest working in a way that better aligns with what we have learnt so far.

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -30,13 +30,33 @@ struct ContentView: View {
 //    @State private var alearMessage = ""
 //    @State private var showingAlert = false
     
-    @State private var recommendedBedtime = ""
+    //@State private var recommendedBedtime = ""
     
     static var defaultWakeTime: Date {
         var components = DateComponents()
         components.hour = 7
         components.minute = 0
         return Calendar.current.date(from: components) ?? Date.now
+    }
+    
+    var recommendedBedtime: String {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            let components = Calendar.current.dateComponents([.hour,.month], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            let prediction = try model.prediction(wake: Double(hour+minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            let sleepTime = wakeUp - prediction.actualSleep
+//            alertTitle = "Bedtime"
+//            alearMessage = "Your bedtime shoud be \(sleepTime.formatted(date: .omitted, time: .shortened))"
+            return "Your bedtime should be \(sleepTime.formatted(date: .omitted, time: .shortened))"
+        } catch {
+            //Error
+//            alertTitle = "Error"
+//            alearMessage = "Something went wrong"
+            return "Something went wrong"
+        }
     }
     
     var body: some View {
@@ -95,10 +115,10 @@ struct ContentView: View {
 //            })
             
             //To Remove the calculate button entirely, I added .onAppear & .onChange
-            .onAppear(perform: {calculateBedtime()})
-            .onChange(of: wakeUp, perform: {_ in calculateBedtime()})
-            .onChange(of: sleepAmount, perform: {_ in calculateBedtime()})
-            .onChange(of: coffeeAmount, perform: {_ in calculateBedtime()})
+//            .onAppear(perform: {calculateBedtime()})
+//            .onChange(of: wakeUp, perform: {_ in calculateBedtime()})
+//            .onChange(of: sleepAmount, perform: {_ in calculateBedtime()})
+//            .onChange(of: coffeeAmount, perform: {_ in calculateBedtime()})
         }
 //        VStack {
 //            Stepper ("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
@@ -109,26 +129,26 @@ struct ContentView: View {
 //        }
     }
     
-    func calculateBedtime() {
-        do {
-            let config = MLModelConfiguration()
-            let model = try SleepCalculator(configuration: config)
-            let components = Calendar.current.dateComponents([.hour,.month], from: wakeUp)
-            let hour = (components.hour ?? 0) * 60 * 60
-            let minute = (components.minute ?? 0) * 60
-            let prediction = try model.prediction(wake: Double(hour+minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
-            let sleepTime = wakeUp - prediction.actualSleep
-//            alertTitle = "Bedtime"
-//            alearMessage = "Your bedtime shoud be \(sleepTime.formatted(date: .omitted, time: .shortened))"
-            recommendedBedtime = "Your bedtime should be \(sleepTime.formatted(date: .omitted, time: .shortened))"
-        } catch {
-            //Error
-//            alertTitle = "Error"
-//            alearMessage = "Something went wrong"
-            recommendedBedtime = "Something went wrong"
-        }
-        //showingAlert = true
-    }
+//    func calculateBedtime() {
+//        do {
+//            let config = MLModelConfiguration()
+//            let model = try SleepCalculator(configuration: config)
+//            let components = Calendar.current.dateComponents([.hour,.month], from: wakeUp)
+//            let hour = (components.hour ?? 0) * 60 * 60
+//            let minute = (components.minute ?? 0) * 60
+//            let prediction = try model.prediction(wake: Double(hour+minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+//            let sleepTime = wakeUp - prediction.actualSleep
+////            alertTitle = "Bedtime"
+////            alearMessage = "Your bedtime shoud be \(sleepTime.formatted(date: .omitted, time: .shortened))"
+//            recommendedBedtime = "Your bedtime should be \(sleepTime.formatted(date: .omitted, time: .shortened))"
+//        } catch {
+//            //Error
+////            alertTitle = "Error"
+////            alearMessage = "Something went wrong"
+//            recommendedBedtime = "Something went wrong"
+//        }
+//        //showingAlert = true
+//    }
     
     
 }
