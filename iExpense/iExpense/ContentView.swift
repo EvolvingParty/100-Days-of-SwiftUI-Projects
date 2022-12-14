ccdc1903e425911f58832f1258e3f18ad4e73794
Kurt <kurtanthonylane@hotmail.com>
2022-12-14 15:25:27 +1000

    DAY 37
    Project 7, part 2

---
+++ iExpense/iExpense/ContentView.swift
@@ -0,0 +1,58 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Kurt Lane on 13/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var expenses = Expenses()
+    @State private var showingAddExpense = false
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                                .font(.callout)
+                                .foregroundColor(.secondary)
+                        }.padding(EdgeInsets(top: 1, leading: 0, bottom: 1, trailing: 0))
+                        Spacer()
+                        Text(item.amount, format: .currency(code: "USD"))
+                    }
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button {
+//                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5.0)
+//                    expenses.items.append(expense)
+                    showingAddExpense = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
+            }
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Kurt Lane on 13/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
