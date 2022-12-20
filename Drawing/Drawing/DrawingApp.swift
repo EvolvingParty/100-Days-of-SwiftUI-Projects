8fbac70612f66ad7e24b70bd3fa7d0f6fe4e030e
Kurt <kurtanthonylane@hotmail.com>
2022-12-20 22:06:29 +1000

    Drawing: Project 9, part 1
    DAY 43

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Kurt Lane on 20/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
